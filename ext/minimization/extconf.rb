758ae7b9df66940b0ee41865dd9202e73e57d53f
lasandun <lasandun@gmail.com>
2014-08-17 02:30:19 +0530

    extconf completed. unwanted files removed

--- ext/minimization/extconf.rb
+++ ext/minimization/extconf.rb
@@ -3,6 +3,7 @@ require 'mkmf'
 # Stops the installation process if one of these commands is not found in
 # $PATH.
 find_executable('make')
+find_executable('gcc')
 
 # Create a dummy extension file. Without this RubyGems would abort the
 # installation process. On Linux this would result in the file "wat.so"
@@ -11,10 +12,11 @@ find_executable('make')
 # Normally the generated Makefile would take care of this but since we
 # don't generate one we'll have to do this manually.
 #
-File.touch(File.join(Dir.pwd, 'dummy.' + RbConfig::CONFIG['DLEXT']))
+#FileUtils.touch(File.join(Dir.pwd, 'dummy.' + RbConfig::CONFIG['DLEXT']))
+#exec 'make'
+exec 'cd ../../lib/opencl/ ; make'
 
 # This is normally set by calling create_makefile() but we don't need that
 # method since we'll provide a dummy Makefile. Without setting this value
 # RubyGems will abort the installation.
 $makefile_created = true
-	true
